6f7fd72e20bdb11903d7275207fb5fb03dc96ecc
Jens Dittrich <jens.dittrich@bigdata.uni-saarland.de>
2026-01-20 14:27:44 +0100

    cleanup

--- tests/first_test.py
+++ tests/first_test.py
@@ -5,20 +5,23 @@ from lib.core import DictionaryAttributeFunction
 
 def test_DictionaryAttributeFunction():
     daf = DictionaryAttributeFunction()
-    assert daf.x == 0
-    assert daf.y == 0
 
     # accessing a non-existing attribute raises AttributeError
     with pytest.raises(AttributeError):
         assert daf.z == 0
 
-    daf.x = 42
-    assert daf.x == 42
+    # check existing attributes:
     assert "x" in daf
     assert "y" in daf
     assert "z" not in daf
+    assert daf.x == 0
+    assert daf.y == 0
 
-    # create a new attribute:
+    # check assigning to an existing attribute:
+    daf.x = 42
+    assert daf.x == 42
+
+    # create/assign to a new attribute:
     daf.z = 100
     assert daf.z == 100
     assert "z" in daf
