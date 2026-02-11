5b5f7ae8e32234fdff86235604f78607c94f5490
Jens Dittrich <jens.dittrich@bigdata.uni-saarland.de>
2026-02-11 15:44:09 +0100

    schema

---
+++ fdm/schema.py
@@ -0,0 +1,26 @@
+from typing import Type
+
+from fdm.API import AttributeFunction
+from fql.predicates.constraints import AttributeFunctionConstraint
+
+
+class Schema[Key](AttributeFunction[Key, Type], AttributeFunctionConstraint):
+    """A schema is an attribute function that defines the expected keys and their types for items in a relation."""
+
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, attribute_function: AttributeFunction) -> bool:
+        """Evaluates whether the given attribute_function fulfills the schema."""
+        assert isinstance(attribute_function, AttributeFunction)
+
+        # check all keys in the schema are present in the attribute function and their types are compatible
+        for item in attribute_function:
+            if item.key not in self:
+                return False
+            if not issubclass(item.value, self[item.key]):
+                return False
+        return True
+
+
+# TODO: write test
